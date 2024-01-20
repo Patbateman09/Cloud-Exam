5df657040b18cf63663ad4a235a0495313d23022
Patbateman09 <135843247+Patbateman09@users.noreply.github.com>
2024-01-20 15:43:55 +0530

    Update exm.py

--- exm.py
+++ exm.py
@@ -1,1 +1,11 @@
-print("Hello World")
+a=int(input("Enter the number: "))
+temp = a
+rev = 0
+while temp > 0:
+    r = temp % 10
+    rev = (rev * 10) + r
+    temp = temp // 10
+if a == rev:
+  print('Palindrome')
+else:
+  print("Not Palindrome")
